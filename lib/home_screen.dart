ee0928d02b9bd4b9130a9002107ddcaf2932c2eb
HP <illailanadiah@gmail.com>
2025-05-04 19:12:08 +0800

    side bar UI

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -21,14 +21,78 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-            child: Text('${user!.email}'),
-          ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: (() => signout()),
-        child: const Icon(Icons.login_rounded),
+    return AnimatedContainer(
+      transform: Matrix4.translationValues(xOffset, yOffset, 0)
+        ..scale(isDraweOpen ? 0.8 : 1)
+        ..rotateZ(isDraweOpen ? -0.5 : 0),
+      duration: const Duration(milliseconds: 200),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: 
+        isDraweOpen ? BorderRadius.circular(40): BorderRadius.circular(0),
+      ),
+      child: SingleChildScrollView(
+        child: Column( 
+          children: [ 
+            <Widget> [
+              SizedBox( 
+                height: 50,
+              ),
+              Container( 
+                margin: EdgeInsets.symmetric( 
+                  horizontal: 20
+                ),
+                child: Row( 
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[ 
+                    isdrawerOpen ? 
+                    GestureDetector( 
+                      child: Icon(Icons.arrow_back_ios_new_rounded), 
+                      onTap: (() { 
+                        setState(() { 
+                          isDraweOpen = false;
+                          xOffset = 0;
+                          yOffset = 0;
+                        });
+                      },
+                    )
+                    : GestureDetector( 
+                      child: Icon(Icons.menu), 
+                      onTap: () { 
+                        setState(() { 
+                          isDraweOpen = true;
+                          xOffset = 290;
+                          yOffset = 80;
+                        });
+                      },
+                    ),
+                    Text(
+                      'Beautiful Drawer',
+                      style: TextStyle( 
+                        fontSize: 20,
+                        color: Colors.black,
+                        decoration: TextDecoration.none),
+                    ),
+                    Container(),
+                  ],
+                ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+      
+      
+      
+        /*child: Center(
+          child: Text('Hello \n ${user!.email}'),
+        ),*/
       ),
     );
+    /*floatingActionButton: FloatingActionButton(
+        onPressed: (() => signout()),
+        child: const Icon(Icons.login_rounded),
+      ),*/
   }
 }
+      
+
